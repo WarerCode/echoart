7a018e7b080e6f83437ca2657d3c4e9ff7eb9525
Jane Sinitsyn <sinitsyn.zena@yandex.ru>
2025-07-07 17:06:15 +0400

    Made ConsoleImage class + Effects class

---
+++ core/ConsoleImage.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import math
+from rich.console import Console
+from core.Effects import Effects
+
+class ConsoleImage:
+    gradient = " .:!/r(l1Z4H9W8$@"
+    syblos_per_pixel = 2
+    max_width = 60
+
+    def __init__(self, path: str="assets/image.jpg"):
+        self.path = path
+
+
+    def display(
+            self, 
+            effects: dict={
+                "negative": False, 
+                "shift": False,
+                "gray": False,
+            }
+        ):
+        console = Console()
+
+        # Открываем изображение
+        print(self.path)
+        img = Image.open(self.path)
+        
+        n = len(self.gradient)
+
+        # Преобразуем в RGB (если это не RGB уже)
+        img = img.convert("RGB")
+
+        # Получаем ширину и высоту
+        width, height = img.size
+        k = width/self.max_width
+
+        for y in range(0, height, math.ceil(k)):
+            for x in range(0, width, math.ceil(k)):
+                pixel = img.getpixel((x, y))
+                if effects.get("negative"):
+                    pixel = Effects.negative(pixel)
+                if effects.get("gray"):
+                    pixel = Effects.gray(pixel)
+                r, g, b = pixel
+                brightness = sum(pixel)/(3*255)
+                symbol = self.gradient[math.floor((n-1)*brightness)]
+                if brightness > 0.75:
+                    console.print(self.syblos_per_pixel*f"[bold rgb({r},{g},{b})]{symbol}[/]", end="")
+                else:
+                    console.print(self.syblos_per_pixel*f"[rgb({r},{g},{b})]{symbol}[/]", end="")
+            print()
