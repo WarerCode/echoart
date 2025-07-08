5351511343fb7749e5b5806f373f65a255e4d451
Jane Sinitsyn <sinitsyn.zena@yandex.ru>
2025-07-08 12:16:56 +0400

    Optimized image.display() + created requirements.txt

--- core/ConsoleImage.py
+++ core/ConsoleImage.py
@@ -1,3 +1,4 @@
+import numpy as np
 from PIL import Image
 import math
 from rich.console import Console
@@ -23,7 +24,6 @@ class ConsoleImage:
         console = Console()
 
         # Открываем изображение
-        print(self.path)
         img = Image.open(self.path)
         
         n = len(self.gradient)
@@ -35,7 +35,10 @@ class ConsoleImage:
         width, height = img.size
         k = width/self.max_width
 
+        text_image = []
+
         for y in range(0, height, math.ceil(k)):
+            row = []
             for x in range(0, width, math.ceil(k)):
                 pixel = img.getpixel((x, y))
                 if effects.get("negative"):
@@ -43,10 +46,18 @@ class ConsoleImage:
                 if effects.get("gray"):
                     pixel = Effects.gray(pixel)
                 r, g, b = pixel
-                brightness = sum(pixel)/(3*255)
+                brightness = Effects.brightness(pixel)
                 symbol = self.gradient[math.floor((n-1)*brightness)]
                 if brightness > 0.75:
-                    console.print(self.syblos_per_pixel*f"[bold rgb({r},{g},{b})]{symbol}[/]", end="")
+                    row.append(f"[bold rgb({r},{g},{b})]{symbol}[/]")
                 else:
-                    console.print(self.syblos_per_pixel*f"[rgb({r},{g},{b})]{symbol}[/]", end="")
-            print()
+                    row.append(f"[rgb({r},{g},{b})]{symbol}[/]")
+            text_image.append(row)
+
+        text_image = np.array(text_image)
+        text_image = np.char.multiply(text_image, 2)
+        length = text_image.size
+        text_image[:, -1] += "\n"
+        text_image = text_image.reshape(length)
+        print_str = "".join(text_image)
+        console.print(print_str)
